52b8cfe30d930af029709f4f4a19e8031ecc5ac7
Parin Patel <parinpatel@Parins-MacBook-Pro.local>
2021-04-04 11:47:41 -0400

    Updated Lab 02

--- app/src/main/java/com/example/intentdemo_patelp/MainActivity.java
+++ app/src/main/java/com/example/intentdemo_patelp/MainActivity.java
@@ -1,54 +1,55 @@
 package com.example.intentdemo_patelp;
 
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int REQUEST_CODE = 1;
+
     private Button button;
     private Intent i;
-    private EditText editText;
-
-
+    private EditText text;
+    public static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.plain_text_input);
         button = findViewById(R.id.button);
-
         i = new Intent(this, SecondActivity.class);
 
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                String s = editText.getText().toString();
-                i.putExtra("Welcome to Droid!", s);
-
-                startActivity(i);
-                    startActivityForResult(i, REQUEST_CODE);
+            public void onClick(View v) {
+                String s = text.getText().toString();
+                i.putExtra("testString", s);
+                // startActivity(i);
+                startActivityForResult(i, REQUEST_CODE);
             }
-        }
-
+        });
     }
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent date){
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
             Bundle extras = data.getExtras();
-            if (extras != null){
-                ConstraintLayout currentLayout = findViewById(R.id.main_layout);
+
+            if(extras != null){
+                int imageID = extras.getInt("imageID");
+                ConstraintLayout currentLayout = findViewById(R.id.MainActivity);
+                currentLayout.setBackground(getDrawable(imageID));
             }
         }
     }
